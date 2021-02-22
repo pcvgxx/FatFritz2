1a9c6ec20fe4aa930751ba72cc68cd25f21b9c43
pcvgxx <79429674+pcvgxx@users.noreply.github.com>
2021-02-22 22:12:35 +0800

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -44,6 +44,7 @@
 //     const unsigned char *const gEmbeddedNNUEEnd;     // a marker to the end
 //     const unsigned int         gEmbeddedNNUESize;    // the size of the embedded file
 // Note that this does not work in Microsoft Visual Studio.
+#define NNUE_EMBEDDING_OFF
 #if !defined(_MSC_VER) && !defined(NNUE_EMBEDDING_OFF)
   INCBIN(EmbeddedNNUE, EvalFileDefaultName);
 #else
